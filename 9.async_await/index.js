c69c8b712c59cf6b3732edba136487dc49b0c7ff
Uday Kumar Purbey <chandanpurbey754@gmail.com>
2025-03-20 08:31:06 +0530

    added

--- 9.async_await/index.js
+++ 9.async_await/index.js
@@ -14,5 +14,20 @@ async function delayedGreet(name) {
 delayedGreet('Uday Kumar Purbey');
 
 
-// 01:29:47
-// mkdir 10.event_emitter
+async function division (num1 , num2 ) {
+  try {
+    if(num2 == 0) throw new Error('Can not divide by 0');
+    return num1 / num2 ;
+    
+  } catch (error) {
+    console.error('Error : ' , error)
+    return null;
+  }
+}
+
+async function mainFn() {
+  console.log( await division(10 , 2))
+  console.log( await division(10 , 0))
+}
+
+mainFn();
