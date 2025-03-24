57660908b4b1627257bf41b5713cdcdce89cf9a1
Uday Kumar Pubey <uday.naxtre@gmail.com>
2025-03-24 19:07:22 +0530

    added

---
+++ 22.concepts/1.event_loop.js
@@ -0,0 +1,52 @@
+/* 
+micro task :process.nextTick , promises 
+
+macro task : setTimeout , setInterval 
+ */
+
+
+// timers  --> pending callbacks  -->  idle , prepare  -->  poll  --> check  --> close callbacks
+
+const fs = require('fs');
+const crypto = require('crypto');
+
+
+console.log('1. Script Started');
+
+
+setTimeout(() => {
+  console.log('2. setTimeout 0 sec Callback (macrotask)');
+}, 0);
+
+setTimeout(() => {
+  console.log('3. setTimeout 0 sec Callback (macrotask)');
+}, 0);
+
+
+setImmediate(() => {
+  console.log('4. setImmediate Callback (check)');
+});
+
+
+Promise.resolve().then(() => {
+  console.log('5. Promise resolved (microtask)');
+});
+
+
+process.nextTick(() => {
+  console.log('6. process.nextTick Callback (microtask)');
+});
+
+
+fs.readFile(__filename , () => {
+  console.log('7. File read Operation (I/O callback)');
+});
+
+
+crypto.pbkdf2('secrest' , 'salt',10000 , 64 , 'sha512' , (err , key) => {
+  if(err) throw err;
+  console.log('8. pbkdf2 Operation (CPU Intensive task)');
+});
+
+
+console.log("9. Scripts Ends...");
---
+++ 22.concepts/3.stream.js
@@ -0,0 +1,46 @@
+//readable  --> use for read
+// writable --> use for write
+// duplex --> use for both read and write (TCP Socket)
+// transform --> zlib streams
+
+
+const fs = require('fs');
+const zlib = require('zlib');
+const crypto = require('crypto');
+const { Transform } = require('stream')
+
+class EncryptString extends Transform {
+  constructor(key , vector) {
+    super();
+    this.key = key;
+    this.vector = vector;
+  }
+
+
+  _transform(chunk , encoding , callbackFn) {
+    const cipher = crypto.createCipheriv('aes-256-cbc' , this.key , this.vector);
+    const encrypted = Buffer.concat([cipher.update(chunk) , cipher.final()]);
+    this.push(encrypted);
+    callbackFn();
+  }
+}
+
+
+const key = crypto.randomBytes(32);
+const vector = crypto.randomBytes(16);
+
+const readableStream = fs.createReadStream('./3.input.txt');
+
+// create new gzip object 
+
+const gzipStream = zlib.createGzip();
+
+const encryptSteam = new EncryptString(key, vector);
+
+const writableStream = fs.createWriteStream('./3.output.txt.gz.enc');
+
+// read --> compress --> encrypt --> write 
+
+readableStream.pipe(gzipStream).pipe(encryptSteam).pipe(writableStream);
+
+console.log('Streaming --> Encrypting --> Writting Data');
