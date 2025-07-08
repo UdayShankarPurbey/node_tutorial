fc21414e9b27e238b966b821ce46248eef3392a1
Rudara Info World <purbeyudaykumar@gmail.com>
2025-07-08 14:34:05 +0530

    added

---
+++ 25.social_media_microservices/media-service/src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+const cloudinary = require("cloudinary").v2;
+const logger = require("./logger");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadMediaTocloudinary = (file) => {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      {
+        resource_type: "auto",
+        // folder: 'media'
+      },
+      (error, result) => {
+        if (error) {
+          logger.error("Error While Uploading Media to Cloudinary", error);
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+
+    uploadStream.end(file.buffer);
+  });
+};
+
+
+module.exports = uploadMediaTocloudinary;
